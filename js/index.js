11f87d3ac308f826d01cc995d4812943eeb3ab4f
Fabrizio Giuseppe Pignatelli <141920930+FabrizioPignatelli02@users.noreply.github.com>
2024-01-06 12:33:57 +0100

    player on homepage done for last episode

--- js/index.js
+++ js/index.js
@@ -19,6 +19,119 @@ function ultimoEpisodio() {
       button.innerText = "PLAY NOW";
       button.classList.add("button-style");
 
+      button.addEventListener("click", playSong);
+
+      function playSong() {
+        if (currentAudio) {
+          currentAudio.pause();
+          currentAudio.remove();
+        }
+        const podcastRange = document.querySelector(".podcast-range");
+
+        podcastRange.value = 0;
+        const audio = document.createElement("audio");
+
+        audio.src = podcast.playback_url;
+
+        currentAudio = audio;
+
+        currentAudio.volume = 0.5;
+
+        currentAudio.play();
+
+        const songsTitle = document.getElementsByClassName("songsTitle")[0];
+        const songsArtist = document.getElementsByClassName("songsArtist")[0];
+        const playerImageSong = document.getElementById("playerImageSong");
+
+        playerImageSong.src = podcast.image_original_url;
+        songsTitle.innerText = podcast.title;
+        songsArtist.innerText = "Fabrizio Pignatelli & Sara Smera";
+
+        const playbar = document.getElementById("playbar");
+        playbar.classList.remove("d-none");
+        playbar.classList.add("d-block");
+
+        const volumeRange = document.querySelector(".volume-range");
+
+        volumeRange.addEventListener("input", function () {
+          currentAudio.volume = volumeRange.value / 100;
+        });
+
+        const totalTimeAudio = document.querySelector(".totalTimeAudio");
+
+        let durationMinute = Math.floor(podcast.duration / 1000 / 60);
+        let durationSecond = Math.floor((podcast.duration / 1000) % 60);
+        if (durationSecond < 10) {
+          durationSecond = "0" + durationSecond;
+        }
+        if (durationMinute < 10) {
+          durationMinute = "0" + durationMinute;
+        }
+        totalTimeAudio.innerText = durationMinute + ":" + durationSecond;
+        console.log("Totale:", podcast.duration);
+
+        podcastRange.addEventListener("change", function () {
+          const currentTimeAudio = document.querySelector(".currentTimeAudio");
+          let actualTime = currentAudio.duration * (podcastRange.value / 100);
+          currentAudio.currentTime = actualTime;
+          console.log("Actual:", actualTime);
+          podcastRange.value = (actualTime / currentAudio.duration) * 100;
+          let minutes = Math.floor(actualTime / 60);
+          let seconds = Math.floor(actualTime % 60);
+          if (minutes < 10) {
+            minutes = "0" + minutes;
+          }
+          if (seconds < 10) {
+            seconds = "0" + seconds;
+          }
+          currentTimeAudio.innerText = minutes + ":" + seconds;
+        });
+
+        setInterval(() => {
+          const currentTimeAudio = document.querySelector(".currentTimeAudio");
+          podcastRange.value = (currentAudio.currentTime / currentAudio.duration) * 100;
+          console.log("Dove sono:", podcastRange.value);
+          let minutes = Math.floor(currentAudio.currentTime / 60);
+          let seconds = Math.floor(currentAudio.currentTime % 60);
+          if (minutes < 10) {
+            minutes = "0" + minutes;
+          }
+          if (seconds < 10) {
+            seconds = "0" + seconds;
+          }
+          currentTimeAudio.innerText = minutes + ":" + seconds;
+        }, 1000);
+
+        positionPodcastRange = 0;
+        positionPodcastRange = (currentAudio.currentTime * 100) / currentAudio.duration;
+        podcastRange.value = positionPodcastRange;
+
+        const playButtonPreview = document.getElementById("playButton");
+        const pauseButtonPreview = document.getElementById("pauseButton");
+
+        playButtonPreview.addEventListener("click", play);
+        pauseButtonPreview.addEventListener("click", pause);
+
+        playButtonPreview.classList.add("d-none");
+        pauseButtonPreview.classList.remove("d-none");
+        pauseButtonPreview.classList.add("d-block");
+
+        function play() {
+          currentAudio.play();
+          playButtonPreview.classList.add("d-none");
+          pauseButtonPreview.classList.remove("d-none");
+          pauseButtonPreview.classList.add("d-block");
+        }
+
+        function pause() {
+          currentAudio.pause();
+          playButtonPreview.classList.remove("d-none");
+          pauseButtonPreview.classList.add("d-none");
+        }
+      }
+
+      let currentAudio;
+
       const h1 = document.createElement("h1");
       h1.innerText = "LAST RELEASED❗";
 
